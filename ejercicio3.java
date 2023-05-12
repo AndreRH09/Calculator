9635f83f7d2193f13f8a03c5689878f5d495929a
fvilcaqui <fvilcaqui@unsa.edu.pe>
2023-05-12 15:06:26 -0500

    ejercicio3 concluido

---
+++ ejercicio3.java
@@ -0,0 +1,10 @@
+public class ejercicio3{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(system.in);
+		String name;
+		System.out.println("Ingresar usuario");
+		name = sc.next();
+                System.out.println("Hola" + name);
+
+	}
+}
